4f614b69c0494e1f7efea1c408f38c037ec0f879
woojinzz <130372814+woojinzz@users.noreply.github.com>
2024-04-21 11:03:12 +0900

    새로운 게시판

---
+++ src/example/JAM/dto/Article.java
@@ -0,0 +1,15 @@
+package example.JAM.dto;
+
+public class Article {
+	public int id;
+	public String title;
+	public String body;
+	
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+		
+
+}
